7ddeec52e5860c5e245f04c3db83dd8dfd86a304
juhi agarwal <juhiagl8@gmail.com>
2022-12-07 12:22:15 +0530

    Update whitelistApis.ts

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -1948,7 +1948,7 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
-      '/proxies/v8/player/questionset/v1/retake': {
+      '/proxies/v8/player/questionset/v1/retake/:id': {
             checksNeeded: [CHECK.ROLE],
             // tslint:disable-next-line: object-literal-sort-keys
             ROLE_CHECK: [
